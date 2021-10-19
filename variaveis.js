859d2ea624bbcf785bf3b9f5ab5a7242bf4f6ba9
Allana Caldas <allanacaldas@gmail.com>
2021-10-19 15:11:52 -0300

    Conhecendo a variavel var

---
+++ variaveis.js
@@ -0,0 +1,11 @@
+//var
+
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area);
+var area;
+
+/*O JS lê todo o programa, carrega as variáveis do tipo var primeiro. Por isso, é possível que declaremos a variável somente na linha 8 e 
+mesmo assim ela funciona normalmente e antecipadamente na linha 6*/
