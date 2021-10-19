be8b8e387549c6849f575495e157035f40bd6279
Allana Caldas <allanacaldas@gmail.com>
2021-10-19 15:34:06 -0300

    Conhecendo o tipo Boolean

---
+++ tipoBoolean.js
@@ -0,0 +1,11 @@
+const primeiroNumero = 5;
+const segundoNumero = "5";
+
+//Comparações retornam true ou false
+console.log(primeiroNumero === segundoNumero);
+
+
+const texto1 = "Alura";
+const texto2 = "A";
+console.log(texto1 === texto2);
+
