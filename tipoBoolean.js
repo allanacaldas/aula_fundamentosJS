b5fb2c7d79ee1f072954f85fa8d9ad40edff76c4
Allana Caldas <allanacaldas@gmail.com>
2021-10-19 16:13:56 -0300

    Conhecendo o null e o undefined

--- tipoBoolean.js
+++ tipoBoolean.js
@@ -19,3 +19,21 @@ console.log("O resultado da comparação entre 1 e true é: " + (1 == true));
 
 
 console.log("O resultado da comparação entre string vazia e false é: " + (" " == false));
+
+//Null e undefined 
+/*Quando iniciamos uma variável com o let e deixamos ela sem valor nenhum, ela ganha, automaticamente,
+pelo JavaScript o valor de undefined. Ou seja, ela não foi definida, mas, ela pode ser utilizada
+*/
+ let minhaVariavel;
+ let variavelNull = null;
+
+ console.log(minhaVariavel);
+ console.log(variavelNull);
+
+//Typeof é usado pra descobrir o tipo de dado que está sendo guardado naquela variável
+console.log(typeof(variavelNull)); // o null foi criado como um objeto
+console.log(typeof(minhaVariavel));
+
+
+
+
