e590fa2d3809558f345afdfdbae8149b13608a2f
Allana Caldas <allanacaldas@gmail.com>
2021-10-19 15:40:23 -0300

    Conhecendo os conceitos de truthy e false

--- tipoBoolean.js
+++ tipoBoolean.js
@@ -9,3 +9,13 @@ const texto1 = "Alura";
 const texto2 = "A";
 console.log(texto1 === texto2);
 
+//Truthy e Falsy
+
+//1 -> true
+//0 -> false
+
+console.log("O resultado da comparação entre 0 e false é: " + (0 == false));
+console.log("O resultado da comparação entre 1 e true é: " + (1 == true));
+
+
+console.log("O resultado da comparação entre string vazia e false é: " + (" " == false));
