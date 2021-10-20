8fa9b676e02ff3b70512ecd836115ee970c2fcdc
Allana Caldas <allanacaldas@gmail.com>
2021-10-20 11:16:10 -0300

    Conhecendo e se familiarizando com os tipos de erros

---
+++ erros.js
@@ -0,0 +1,10 @@
+//const numero; // dispara o seguinte erro-> SyntaxError: Missing initializer in const declaration
+const numero = 1;
+
+//console.log(numero; // dispara o seguinte erro-> SyntaxError: missing ) after argument list
+console.log(numero);
+
+//console.log(variavelNãoCriada); // ReferenceError: variavelNãoCriada is not defined
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
