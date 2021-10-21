0ab1c08d9e0512c8a3ecba5f38fef8bab55acbdf
Allana Caldas <allanacaldas@gmail.com>
2021-10-21 13:00:21 -0300

    Inserindo comentario sobre o conceito de parametros e argumentos

--- parametros.js
+++ parametros.js
@@ -4,7 +4,11 @@ function soma(num1, num2) {
 
 console.log(soma(65, 78));
 
-//parâmetros x argumentos
+/*parâmetros x argumentos
+Na prática se referem ao mesmo tipo de dado; algumas documentações se referem a parâmetros
+no momento em que a função é definida (no caso, numero1, numero2, etc) e argumentos como os
+ dados que utilizamos para executar a função (ou seja, 30, 45, etc).
+*/
 
 function multiplica(numero1 = 1, numero2 = 1) {
     return numero1 * numero2;
