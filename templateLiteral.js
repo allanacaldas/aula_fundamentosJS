1fb1c5f6c7612cb18831914cd555dab57b4e08f5
Allana Caldas <allanacaldas@gmail.com>
2021-10-21 12:30:20 -0300

    Concatenacao usando Template Strings

---
+++ templateLiteral.js
@@ -0,0 +1,10 @@
+// Forma de concatenar usando o +
+const nome = "Ju";
+const idade = 21;
+
+const frase = "Meu nome é " + nome + " e tenho " + idade + " anos";
+console.log(frase);
+
+//Template String  `${}`
+//Imprimindo a mesma frase usando o Template String
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
